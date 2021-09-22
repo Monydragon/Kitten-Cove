2ffeace658d10a20bc323aa03f0d1d7ae81ed3c3
Mony Dragon <Monydragon@gmail.com>
2021-09-22 02:23:03 -0500

    Installed Cinemachine, Fungus, Setup Main Characters Basic
    Installed Cinemachine, Fungus, and setup the main characters male and female versions and animated the characters and created the scripts related to them. Also started the eventmanager code for interaction.

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager
+{
+	public delegate void D_Void();
+	public delegate void D_GameObject(GameObject go1, GameObject go2);
+	
+	public static event D_GameObject onInteract;
+	
+	public static void Interact(GameObject source, GameObject target) { onInteract.Invoke(source, target); }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+	private float speed = 3f;
+	[SerializeField]
+	private Animator anim;
+
+
+	private Vector3 movement;
+
+    private void OnEnable()
+    {
+        EventManager.onInteract += EventManager_onInteract;
+    }
+
+    private void OnDisable()
+    {
+		EventManager.onInteract -= EventManager_onInteract;
+
+	}
+
+    private void EventManager_onInteract(GameObject source, GameObject target)
+    {
+		Debug.Log($"{source.name} Interacted with {target.name}");
+    }
+
+    void Awake()
+	{
+		anim = GetComponent<Animator>();
+	}
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+	    transform.position += movement * speed * Time.deltaTime;
+	    
+	    if(anim != null)
+	    {
+		    if(movement != Vector3.zero)
+		    {
+			    anim.SetBool("isMoving", true);
+			    anim.SetFloat("InputX",movement.x);
+			    anim.SetFloat("InputY",movement.y);
+		    }
+		    else
+		    {
+			    anim.SetBool("isMoving", false);
+		    }
+	    }
+    }
+    
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		Debug.Log($"You hit something: {col.gameObject.name}");
+		EventManager.Interact(gameObject, col.gameObject);
+	}
+}
